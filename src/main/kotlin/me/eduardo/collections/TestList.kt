13807ba856238614ee9e938d8e03493a53ad5817
Eduardo Yoshizato <eduyoshi@gmail.com>
2021-03-04 17:08:36 -0300

    update test list, extract employed to separated file and created test

---
+++ src/main/kotlin/me/eduardo/collections/TestList.kt
@@ -0,0 +1,27 @@
+package me.eduardo.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0,"CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+    funcionarios.forEach{ println(it) }
+    println("*******************")
+
+    println(funcionarios.find { it.nome == "Maria" })
+    println("*******************")
+
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it)}
+    println("*******************")
+
+    funcionarios
+        .groupBy { it.tipoContrato }
+        .forEach { println(it)}
+    println("*******************")
+
+}
+
+
